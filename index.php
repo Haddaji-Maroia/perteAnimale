c5bb05a895b75f5bdd906b221bbb49bd423cd458
Haddaji-Maroia <maroia.haddaji@student.hepl.be>
2025-02-11 21:36:24 +0100

    added classes and styles.css file
    and also the reset one

--- index.php
+++ index.php
@@ -10,13 +10,15 @@
     <meta name="keywords" content="Keywords">
     <meta name="author" content="Author">
     <title>J'ai perdue mon animal</title>
+    <link rel="stylesheet" href="./CSS/reset.css">
+    <link rel="stylesheet" href="./CSS/styles.css">
 </head>
 <body>
     <h1>Déclaration de perte d'animal</h1>
     <form action="/process.php" method="post">
     <fieldset> 
         <legend>Vos coordonnées</legend>
-        <div>
+        <div class="email">
             <label for="email">Email</label>
             <input type="email"
                    name="email"
@@ -31,7 +33,7 @@
         <?php if (isset($_SESSION['errors']['email'])): ?>
         <div><p><?= $_SESSION['errors']['email'] ?></p></div>
         <?php endif; ?>
-        <div>
+        <div class="retapEmail">
             <label for="vemail">Retapez votre email une seconde fois</label>
             <input type="email"
                    name="vemail"
@@ -46,19 +48,23 @@
         <?php if (isset($_SESSION['errors']['vemail'])): ?>
             <div><p><?= $_SESSION['errors']['vemail'] ?></p></div>
         <?php endif; ?>
-        <label for="number">Téléphone</label>
-        <input type="text" name="number" id="number" placeholder="0499 10 10 10">
-        <label for="pays">Pays</label>
-        <select name="pays" id="pays">
-            <option value="Belgique">Belgique</option>
-            <option value="France">France</option>
-            <option value="Espagne">Espagne</option>
-            <option value="Italie">Italie</option>
-        </select>
+        <div class="telephone">
+            <label for="number">Téléphone</label>
+            <input type="text" name="number" id="number" placeholder="0499 10 10 10">
+        </div>
+        <div class="pays">
+            <label for="pays">Pays</label>
+            <select name="pays" id="pays">
+                <option value="Belgique">Belgique</option>
+                <option value="France">France</option>
+                <option value="Espagne">Espagne</option>
+                <option value="Italie">Italie</option>
+            </select>
+        </div>
     </fieldset>
         <fieldset>
             <legend>Description de l'animal perdu</legend>
-            <div>
+            <div class="typeAnimal">
                 <label for="typeAnimal">Type d'animal</label>
                 <select name="typeAnimal" id="typeAnimal">
                     <option value="">Chien</option>
@@ -67,33 +73,31 @@
                     <option value="">Oiseau</option>
                 </select>
             </div>
-            <div>
+            <div class="nomAnimal">
                 <label for="nom">Nom de l'animal</label>
                 <input type="text" name="nom" id="nom">
             </div>
-            <div>
+            <div class="puce">
                 <label for="puce">Puce (obligatoire si chien)</label>
                 <input type="text" name="puce" id="puce">
             </div>
-            <div>
+            <div class="sexe">
                 <label for="sexe">sexe</label>
-                <div>
                     <input type="radio" name="sexe" id="male" value="male">
                     <label for="sexeM">Male</label>
 
                     <input type="radio" name="sexe" id="femelle" value="femelle">
                     <label for="sexeF">Femelle</label>
-                </div>
             </div>
-            <div>
+            <div class="age">
                 <label for="age">Age (estimation)</label>
                 <input type="text" name="age" id="age">
             </div>
-            <div>
+            <div class="race">
                 <label for="race">Race</label>
                 <input type="text" name="race" id="race">
             </div>
-            <div>
+            <div class="tattoo">
                 <label for="tattoo">Tatouage</label>
                 <select name="tattoo" id="tattoo">
                     <option value="OreilleG">Oreille gauche</option>
@@ -104,30 +108,30 @@
                 <label for="codeT">Code tatouage</label>
                 <input type="text" name="codeT" id="codeT">
             </div>
-            <div>
+            <div class="description">
                 <label for="desciption">Description / Signes particuliers</label>
                 <textarea name="description" id="desciption" cols="30" rows="10"></textarea>
             </div>
-            <div>
+            <div class="photo">
                 <label for="photo">Photo de l'animal</label>
                 <input type="text" name="photo" id="photo">
             </div>
         </fieldset>
         <fieldset>
             <legend>Date et localité de la perte</legend>
-            <div>
+            <div class="date">
                 <label for="date">Date</label>
                 <input type="date" name="date" id="date">
             </div>
-            <div>
+            <div class="time">
                 <label for="heure">Heure</label>
                 <input type="time">
             </div>
-            <div>
+            <div class="codePostal">
                 <label for="codeP">Code postal</label>
                 <input type="text" name="codeP" id="codeP">
             </div>
-            <div>
+            <div class="pays">
                 <label for="pays">Pays</label>
                 <select name="pays" id="pays">
                     <option value="Belgique">Belgique</option>
