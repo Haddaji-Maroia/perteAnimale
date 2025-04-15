38cd2874fc202d660d235a8b3caf7736004e37ba
Haddaji-Maroia <maroia.haddaji@student.hepl.be>
2025-04-15 15:24:34 +0200

    last changement

--- process.php
+++ process.php
@@ -1,5 +1,82 @@
 <?php
 
+
+
+/*
+ * Valider les deux champs
+ */
+$email = '';
+if (array_key_exists('email', $_REQUEST)) {
+    $email = trim($_REQUEST['email']);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $_SESSION['errors']['email'] = 'l email doit etre une email';
+    }
+}else{
+    $_SESSION['errors']['email'] = "L email est requis";
+}
+
+
+/*
+ * S'il y a des erreurs, on redige vers la page du formulaire, en mémorisant le temps
+ * d'une requete les erreurset les anciennes données
+ */
+
+if(!is_null($_SESSION['errors'])){
+    $_SESSION['old'] = $_REQUEST;
+    $_SESSION['old'] = $_REQUEST;
+    header('Location: index.php');
+    exit;
+}
+
+
+/*
+ * Assurer le rendu récapitulatif des données soumise
+ */
+
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Récapitulatif</title>
+</head>
+<body>
+    <h1>Récapitulatif des données soumises</h1>
+    <dl>
+        <div>
+            <dt>Email&nbsp;:</dt>
+            <dt><?= $email ?></dt>
+        </div>
+    </dl>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+/*
 session_start();
 
 // Valider les deux champs
@@ -45,3 +122,4 @@ if (!is_null($_SESSION['errors'])){
 </dl>
 </body>
 </html>
+*/
